071bb162a3e1837fb6a127e179e3462c73de3cf6
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-26 19:40:56 -0600

    after get categories

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
+function iniciarApp() {
 
+    obtenerCategorias();
 
+    function obtenerCategorias() {
+        const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then( resultado => console.log(resultado.categories))
+    }
+}
 
+document.addEventListener('DOMContentLoaded', iniciarApp)
 
 
 
