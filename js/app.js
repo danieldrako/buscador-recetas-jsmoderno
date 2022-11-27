4668136f984a4543f81fab60a858b47c7a362fe5
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-26 19:49:05 -0600

    after show categories in the select

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 function iniciarApp() {
 
+
+    const selectCategorias = document.querySelector('#categorias');
+
     obtenerCategorias();
 
     function obtenerCategorias() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then( resultado => console.log(resultado.categories))
+            .then( resultado => msotrarCategorias(resultado.categories))
+    }
+
+    function msotrarCategorias(categorias = []) {
+        categorias.forEach( categoria => {
+            const { strCategory } = categoria;
+            const option = document.createElement('OPTION');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategorias.appendChild(option);     
+        })
     }
 }
 
